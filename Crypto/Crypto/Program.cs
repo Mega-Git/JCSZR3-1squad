99feeb844028ddda08de4e9bc8e1e497f789eea0
Marcin <marcinnns95@gmail.com>
2021-05-09 13:14:36 +0200

    test1

--- Crypto/Crypto/Program.cs
+++ Crypto/Crypto/Program.cs
@@ -6,7 +6,7 @@ namespace Crypto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("siema!");
         }
     }
 }
