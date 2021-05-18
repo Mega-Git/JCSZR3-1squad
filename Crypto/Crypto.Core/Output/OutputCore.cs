b50c10e37b8ec56f77fd1f1004541a4b830203df
Adam Pietrzak <pietrzakpraca@gmail.com>
2021-05-18 14:26:19 +0200

    Output Core and Search

---
+++ Crypto/Crypto.Core/Output/OutputCore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class OutputCore
+{
+	public string Display(string name)
+	{
+		return $"{name}";
+	}
+
+	public void DisplayValueFromName(string name)
+    {
+		var NewName = Search.SearchValueByName(name);
+		Display(NewName);
+    }
+}
---
+++ Crypto/Crypto.Core/Output/Search.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Search
+{
+	public static string SearchValueByName(string name)
+	{
+        List<String> CurrencyList = new();
+        if (CurrencyList.Contains(name))
+        {
+            var x = CurrencyList.Find(z => z.Contains(name));
+            return x;
+        }
+        else
+        {
+            return "No match";
+        }
+	}
+}
