e066479c3b7956da2f301d5526c1178299cca1c4
Adam Pietrzak <pietrzakpraca@gmail.com>
2021-06-03 13:09:35 +0200

    Templates and buttons

--- Crypto/Crypto.Core/Output/OutputCore.cs
+++ Crypto/Crypto.Core/Output/OutputCore.cs
@@ -2,14 +2,77 @@
 
 public class OutputCore
 {
-	public string Display(string name)
+	public string Display(string value)
 	{
-		return $"{name}";
+		return $"{value}";
 	}
 
-	public void DisplayValueFromName(string name)
+	public string DisplayValueFromName(string name)
     {
-		var NewName = Search.SearchValueByName(name);
-		Display(NewName);
+		var NewName = Search.SearchByName(name);
+		var Value = Search.CurrencyList.Find(x => x.Contains(NewName));
+		return Display(Value);
+    }
+	public void DisplayAllCurrencies()
+    {
+        Search.CurrencyList.ForEach(x => Display(x));
+    }
+	public void DisplayValueFromNameInTimePeriod(string name)
+    {
+        var NewName = Search.SearchByName(name);
+		var Value = Search.CurrencyList.Find(x => x.Contains(NewName));
+        string StartingDate = GetStartingDate();
+        string EndingDate = GetEndingDate();
+        //Search.CurrencyList.FindAll(x => x.Value >= StartingDate && x.Value <= EndingDate) ????????
+    }
+    public string GetStartingDate()
+    {
+        Console.WriteLine("Input starting date: ");
+        Console.Write("Day: ");
+        string StartingDay = Console.ReadLine();
+        Console.Write("Month: ");
+        string StartingMonth = Console.ReadLine();
+        Console.Write("Year: ");
+        string StartingYear = Console.ReadLine();
+        string StartingDate = $"{StartingDay} {StartingMonth} {StartingYear}";
+        return StartingDate;
+    }
+    public string GetEndingDate()
+    {
+        Console.WriteLine("Input ending date: ");
+        Console.Write("Day: ");
+        string EndingDay = Console.ReadLine();
+        Console.Write("Month: ");
+        string EndingMonth = Console.ReadLine();
+        Console.Write("Year: ");
+        string EndingYear = Console.ReadLine();
+        string EndingDate = $"{EndingDay} {EndingMonth} {EndingYear}";
+        return EndingDate;
+    }
+    public void DisplayCurrencyWithHighestValueLoss()
+    {
+        //Search.CurrencyList.Find(x => x.ValueLoss.Max()); ????
+    }
+	public void DisplayValueFromNameButton()
+    {
+		Console.WriteLine("Input currency name: ");
+        string CurrencyName = Console.ReadLine();
+        Display(DisplayValueFromName(CurrencyName));
+    }
+	public void DisplayAllCurrenciesButton()
+    {
+        Console.WriteLine("All currencies: ");
+		DisplayAllCurrencies();
+    }
+    public void DisplayValueFromNameInTimePeriodButton()
+    {
+        Console.WriteLine("Input currency name: ");
+        string CurrencyName = Console.ReadLine();
+        DisplayValueFromNameInTimePeriod(CurrencyName);
+    }
+    public void DisplayCurrencyWithHighestValueLossButton()
+    {
+        Console.WriteLine("Currency with highest value loss: ");
+        DisplayCurrencyWithHighestValueLoss();
     }
 }
--- Crypto/Crypto.Core/Output/Search.cs
+++ Crypto/Crypto.Core/Output/Search.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 public class Search
 {
-	public static string SearchValueByName(string name)
+    static public List<String> CurrencyList = new List<string>();
+	public static string SearchByName(string name)
 	{
-        List<String> CurrencyList = new();
         if (CurrencyList.Contains(name))
         {
             var x = CurrencyList.Find(z => z.Contains(name));
