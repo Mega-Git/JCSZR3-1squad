57a671830c725e58c8c54945654d320902f85905
Adam Pietrzak <pietrzakpraca@gmail.com>
2021-06-15 11:07:36 +0200

    Prevernt user from inputting invalid dates

--- Crypto/Crypto.Core/Output/OutputCore.cs
+++ Crypto/Crypto.Core/Output/OutputCore.cs
@@ -7,15 +7,15 @@ public class OutputCore
 {
 	public static string DisplayValueFromName(string name)
     {
-		var newName = Search.SearchByName(name);
-        if (newName == "No match")
+		var currencyName = Search.SearchByName(name);
+        if (currencyName == "No match")
         {
-            return newName;
+            return currencyName;
         }
         else
         {
-		    var value = JsonFile.CryptoCurrencies.Find(x => x.Currency.Contains(newName)).Prices[0];
-		    return $"{newName} {value}";
+		    var price = JsonFile.CryptoCurrencies.Find(x => x.Currency.Contains(currencyName)).Prices[0];
+		    return $"{currencyName} {price}";
         }
     }
 	public static void DisplayAllCurrencies()
@@ -24,10 +24,10 @@ public class OutputCore
     }
 	public static void DisplayValueFromNameInTimePeriod(string name)
     {
-        var newName = Search.SearchByName(name);
-        if (newName == "No match")
+        var currencyName = Search.SearchByName(name);
+        if (currencyName == "No match")
         {
-            Console.WriteLine(newName);
+            Console.WriteLine(currencyName);
         }
         else
         {
@@ -38,23 +38,23 @@ public class OutputCore
             {
                 userDates.Add(date);
             }
-            var currencyDates = JsonFile.CryptoCurrencies.Where(x => x.Currency.Contains(newName)).SelectMany(x => x.Timestamps).ToList();
-            List<DateTime> currencyDatesFix = currencyDates.Select(x => DateTime.Parse(x)).ToList();
-            var overlappingDates = currencyDatesFix.Select(x => x).Intersect(userDates.Select(y => y)).ToList();
+            var currencyDates = JsonFile.CryptoCurrencies.Where(x => x.Currency.Contains(currencyName)).SelectMany(x => x.Timestamps).ToList();
+            List<DateTime> currencyDatesToDateTime = currencyDates.Select(x => DateTime.Parse(x)).ToList();
+            var overlappingDates = currencyDatesToDateTime.Select(x => x).Intersect(userDates.Select(y => y)).ToList();
             if (overlappingDates.Count == 0)
             {
                 Console.WriteLine("No matches");
             }
             else
             {
-                var datesWithValues = JsonFile.CryptoCurrencies.Where(x => x.Currency.Contains(newName)).Select(x => new {x.Prices, x.Timestamps}).ToList();
+                var datesWithValues = JsonFile.CryptoCurrencies.Where(x => x.Currency.Contains(currencyName)).Select(x => new {x.Prices, x.Timestamps}).ToList();
                 var datesWithValuesTimestamps = datesWithValues.SelectMany(x => x.Timestamps).ToList();
-                var datesWithValuesTimestampsFix = datesWithValuesTimestamps.Select(x => DateTime.Parse(x)).ToList();
+                var datesWithValuesTimestampsToDateTime = datesWithValuesTimestamps.Select(x => DateTime.Parse(x)).ToList();
                 for (int i = 0; i < overlappingDates.Count; i++)
                 {
                     for (int o = 0; o < datesWithValuesTimestamps.Count; o++)
                     {
-                        var dateFind = datesWithValuesTimestampsFix[o].Equals(overlappingDates[i]);
+                        var dateFind = datesWithValuesTimestampsToDateTime[o].Equals(overlappingDates[i]);
                         if (dateFind)
                         {
                             Console.WriteLine($"Time: {datesWithValues[0].Timestamps[o]} Price: {datesWithValues[0].Prices[o]} ");
@@ -65,38 +65,100 @@ public class OutputCore
             }
         }
     }
+
+    public static string GetDay()
+    {
+        Console.WriteLine("Day: ");
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int day))
+            {
+                if (day > 0 && day <= 31)
+                {
+                    return day.ToString();
+                }
+            }
+            Console.WriteLine("Input a valid day: ");
+        }
+    }
+    public static string GetMonth()
+    {
+        Console.WriteLine("Month: ");
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int month))
+            {
+                if (month > 0 && month <= 12)
+                {
+                    return month.ToString();
+                }
+            }
+            Console.WriteLine("Input a valid month: ");
+        }
+    }
+    public static string GetYear()
+    {
+        Console.WriteLine("Year: ");
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                if (year > 0 && year <= DateTime.Today.Year)
+                {
+                    return year.ToString();
+                }
+            }
+            Console.WriteLine("Input a valid year: ");
+        }
+    }
     public static string GetStartingDate()
     {
-        Console.WriteLine("Input starting date: ");
-        Console.Write("Day: ");
-        string startingDay = Console.ReadLine();
-        Console.Write("Month: ");
-        string startingMonth = Console.ReadLine();
-        Console.Write("Year: ");
-        string startingYear = Console.ReadLine();
-        string startingDate = $"{startingYear}-{startingMonth}-{startingDay}";
-        return startingDate;
+        while (true)
+        {
+            Console.WriteLine("Input starting date: ");
+            string startingDay = GetDay();
+            string startingMonth = GetMonth();
+            string startingYear = GetYear();
+            string startingDate = $"{startingYear}-{startingMonth}-{startingDay}";
+            
+            if (!DateTime.TryParse(startingDate, out DateTime date))
+            {
+                Console.WriteLine("Invalid date, try again.");
+            }
+            else
+            {
+                return startingDate;
+            }
+        }
     }
     public static string GetEndingDate()
     {
-        Console.WriteLine("Input ending date: ");
-        Console.Write("Day: ");
-        string endingDay = Console.ReadLine();
-        Console.Write("Month: ");
-        string endingMonth = Console.ReadLine();
-        Console.Write("Year: ");
-        string endingYear = Console.ReadLine();
-        string endingDate = $"{endingYear}-{endingMonth}-{endingDay}";
-        return endingDate;
+        while (true)
+        {
+            Console.WriteLine("Input ending date: ");
+            string endingDay = GetDay();
+            string endingMonth = GetMonth();
+            string endingYear = GetYear();
+            string endingDate = $"{endingYear}-{endingMonth}-{endingDay}";
+            if (!DateTime.TryParse(endingDate, out DateTime date))
+            {
+                Console.WriteLine("Invalid date, try again.");
+            }
+            else
+            {
+                return endingDate;
+            }
+        }
     }
 
     public static string GetCurrencyName()
     {
         Console.WriteLine("Input currency name: ");
         var currencyName = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(currencyName))
+        while (string.IsNullOrWhiteSpace(currencyName))
         {
             Console.WriteLine("Input a valid name: ");
+            currencyName = Console.ReadLine();
         }
 
         return currencyName.ToUpper();
