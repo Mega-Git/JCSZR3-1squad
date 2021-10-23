7e73d27db1ba8da4731b1941b9d4536dfcb300f9
Marcin <marcinnns95@gmail.com>
2021-10-23 13:17:20 +0200

    Add path working on other computer

--- Crypto/Crypto.Core/Providers/NomicsProvider.cs
+++ Crypto/Crypto.Core/Providers/NomicsProvider.cs
@@ -19,7 +19,7 @@ namespace Crypto.Core.Providers
             var client = new HttpClient();
 
             string key = "900135c7b342d5abfe1594e8a6275295376539e3";
-            string URL = $"https://api.nomics.com/v1/currencies/sparkline?key={key}&ids=BTC&start=2021-04-14T00%3A00%3A00Z";
+            string URL = $"https://api.nomics.com/v1/currencies/sparkline?key={key}&ids=BTC,ETH&start=2021-04-14T00%3A00%3A00Z";
             var response = client.GetAsync(URL).Result;
             while (response.StatusCode != HttpStatusCode.OK)
             {
@@ -29,10 +29,11 @@ namespace Crypto.Core.Providers
             var Crypto = JsonConvert.DeserializeObject<List<CurrencyModel>>(response.Content.ReadAsStringAsync().Result);
 
             //Serialize data from API to file in folder
+            var path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Crypto.Core\\jsonfiles\\waluty\\Writes.json");
 
             string json = JsonConvert.SerializeObject(Crypto.ToArray());
 
-            File.WriteAllText(@"D:\CryptoApp\JCSZR3-1squad\Crypto\Crypto.Core\jsonFiles\waluty\writes.json", json);
+            File.WriteAllText(path, json);
 
 
             return Crypto;
